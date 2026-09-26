d7ead43063795f465b0197619b046b428474c5ec
AmirFarahani82 <amyrfrahany427@gmail.com>
2026-09-26 23:20:18 +0330

    feat: add route change progress bar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavPanel from "@/app/components/ui/NavPanel";
 import Providers from "./providers";
 import HabitFormModal from "../components/habit-form/HabitFormModal";
+import NextTopLoader from "nextjs-toploader";
 
 export default function MainLayout({
   children,
@@ -10,9 +11,16 @@ export default function MainLayout({
 }) {
   return (
     <Providers>
+      <NextTopLoader
+        color="#8b5cf6"
+        height={3}
+        showSpinner={false}
+        initialPosition={0.05}
+        crawlSpeed={150}
+      />
       <main className="relative h-screen md:static md:grid md:grid-cols-[180px_minmax(0,2fr)]">
         <NavPanel />
-        <div className="relative min-w-0 pb-16 overflow-y-auto">
+        <div className="relative min-w-0 overflow-y-auto pb-16">
           {children}
           <HabitFormModal />
         </div>
